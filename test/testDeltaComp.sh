a6ec4b53b5271a9b731cf76c219bc13edfe09913
Fabian Klötzl <kloetzl@evolbio.mpg.de>
2014-08-07 09:02:55 +0200

    adds exact delta to test script

--- test/testDeltaComp.sh
+++ test/testDeltaComp.sh
@@ -1,5 +1,5 @@
 echo COMMENT Simulate diploid genome
-ms 2 1 -t 10000 -r 1000 200000 | ms2dna > template.fasta
+ms 2 1 -t 10000 -r 8000 1600000 | ms2dna > template.fasta
 echo COMMENT Simulate genome sequencing
 python makedata.py template.fasta 10 > /dev/null
 echo COMMENT Convert sequencer reads to BAM file
@@ -18,3 +18,4 @@ mlRho -m 1 -M 5 | grep '^[1-9]' | awk '{print $1 "\t" $4}' | perl -pe 's/</\t/g'
 echo COMMENT Run Delta24
 echo Distance   Delta
 /usr/bin/time -f "%e %M" ../src/Delta24 test.bam 1 6 1 | grep '^D=' | perl -pe 's/=/\t/g;s/,//g' | awk '{print $2 "\t" $4}'
+delta template.fasta -d 1 -D 5 | cut -f 1,6
