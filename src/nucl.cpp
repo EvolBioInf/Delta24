40a5ad4d7638b57181eed3d669864fef9f71495a
Fabian Klötzl <kloetzl@evolbio.mpg.de>
2014-08-25 11:02:36 +0200

    adds docs for Nucl class

--- src/nucl.cpp
+++ src/nucl.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 
+/** @brief Map `{A,C,G,T}` to a two bit code. */
 size_t Nucl::char2code( char c){
 	size_t ret = 0;
 	switch( c){
@@ -13,7 +14,7 @@ size_t Nucl::char2code( char c){
 	return ret;
 }
 
-
+/** @brief Map the two bit code back into the nucleotide. */
 char Nucl::code2char( std::size_t d){
 	char c = 0;
 	switch( d & 0x3UL){
@@ -25,10 +26,15 @@ char Nucl::code2char( std::size_t d){
 	return c;
 }
 
+/** @brief Construct a new nucleotide.
+ * @param readID - The hash of the read this nucleotide belongs to.
+ * @param c - The character {A,C,G,T}.
+ */
 Nucl::Nucl(size_t readID, char c): data(readID<<2) {
 	data |= char2code(c);
 }
 
+/** @brief Default constructor */
 Nucl::Nucl(): data(0){};
 
 bool Nucl::operator< (const Nucl& b) const {
--- src/nucl.hpp
+++ src/nucl.hpp
@@ -5,6 +5,11 @@
 
 using std::size_t;
 
+/** @brief an abstraction of nucleotides.
+ *
+ * This class contains nucleotide data. It keeps both, the character `{A,C,G,T}`
+ * and the read it came from in a 64bit value.
+ */
 class Nucl {
 private:
 	size_t data;
@@ -16,10 +21,12 @@ public:
 	bool operator< (const Nucl&) const;
 	bool operator> (const Nucl&) const;
 
+	/** @brief Extract the character code. */
 	size_t getCode() const{
 		return data & 0x3UL;
 	}
 
+	/** @brief Extract the read ID. */
 	size_t getReadID() const {
 		return data & (~0UL ^ 0x3UL);
 	}
